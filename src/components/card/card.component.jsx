685cf362871704f72633c9123b97d49c76773092
Andrii Bespalko <bellumm@gmail.com>
2020-09-15 16:18:43 +0300

    Stylizes Card component; Adds image and email there

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,3 +1,11 @@
 import React from "react";
+import "./card.styles.css";
 
-export const Card = props => <div><h1>{props.user.name}</h1></div>;
+
+export const Card = props => (
+  <div className="card-container">
+    <img src={`https://robohash.org/${props.user.id}?set=set4&size=360x360`} alt={`${props.user.name}`}/>
+    <h2>{props.user.name}</h2>
+    <p>{props.user.email}</p>
+  </div>
+);
