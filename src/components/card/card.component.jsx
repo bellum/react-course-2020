5cecaf3bbcca6eae1481d78d17342e36af25f8d2
Andrii Bespalko <bellumm@gmail.com>
2020-09-15 17:41:12 +0300

    Uses object destructuring inside Card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./card.styles.css";
 
 
-export const Card = props => (
+export const Card = ({user}) => (
   <div className="card-container">
-    <img src={`https://robohash.org/${props.user.id}?set=set4&size=360x360`} alt={`${props.user.name}`}/>
-    <h2>{props.user.name}</h2>
-    <p>{props.user.email}</p>
+    <img src={`https://robohash.org/${user.id}?set=set4&size=360x360`} alt={`${user.name}`}/>
+    <h2>{user.name}</h2>
+    <p>{user.email}</p>
   </div>
 );
