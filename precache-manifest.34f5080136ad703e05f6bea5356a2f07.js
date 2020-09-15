f351eaccbc9c541ca77b6f1ee219e03f83c7ba37
Andrii Bespalko <bellumm@gmail.com>
2020-09-15 19:07:50 +0300

    Updates

---
+++ precache-manifest.34f5080136ad703e05f6bea5356a2f07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8144213b2a069c38e2ada553cfad56de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cc107490a35d0f53db6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35266a3ea0338aa51723",
+    "url": "/static/js/2.5566d5f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5566d5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc107490a35d0f53db6",
+    "url": "/static/js/main.47964f5f.chunk.js"
+  },
+  {
+    "revision": "dd96d35a8273ec041535",
+    "url": "/static/js/runtime-main.563c85e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
